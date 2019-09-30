2659e13553d43043389d870f8a216a9adac83655
lembolov9 <tomas080808>
2019-10-01 00:58:04 +0300

    Add ItemService

--- item-service/src/main/java/com/microservices/item/controller/ItemController.java
+++ item-service/src/main/java/com/microservices/item/controller/ItemController.java
@@ -2,35 +2,46 @@ package com.microservices.item.controller;
 
 
 import com.microservices.item.entity.ItemEntity;
-import com.microservices.item.repository.ItemRepository;
+import com.microservices.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
-    private final ItemRepository itemRepository;
+    private final ItemService itemService;
     @Autowired
-    public ItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
     }
 
     @GetMapping
     public List<ItemEntity> list() {
-        return itemRepository.findAll();
+        return itemService.list();
     }
 
     @GetMapping("{id}")
-    public ItemEntity getOne(@PathVariable("id") ItemEntity item){
-        return item;
+    public Optional<ItemEntity> getOne(@PathVariable long id){
+        return itemService.getOne(id);
     }
 
     @PostMapping
     public ItemEntity create(@Valid @RequestBody ItemEntity item) {
-        return itemRepository.save(item);
+        return itemService.create(item);
+    }
+
+    @PutMapping("{id}/increasing/{amount}")
+    public ItemEntity increaseAmount(@PathVariable long amount, @PathVariable long id) {
+        return itemService.increaseAmount(id, amount);
+    }
+
+    @PutMapping("{id}/decreasing/{amount}")
+    public ItemEntity decreaseAmount(@PathVariable long amount, @PathVariable long id) {
+        return itemService.decreaseAmount(id, amount);
     }
 
 }
--- item-service/src/main/java/com/microservices/item/repository/ItemRepository.java
+++ item-service/src/main/java/com/microservices/item/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.microservices.item.repository;
 
 import com.microservices.item.entity.ItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+public interface ItemRepository extends CrudRepository<ItemEntity, Long> {
+
 }
