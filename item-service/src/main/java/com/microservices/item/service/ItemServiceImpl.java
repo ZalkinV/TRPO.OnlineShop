786580a98a88a28d87b9c2583cb02e922f6eba2d
lembolov9 <tomas080808>
2019-10-04 14:15:40 +0300

    Add Logger for ItemService operations Add Swagger for Service

--- item-service/src/main/java/com/microservices/item/service/ItemServiceImpl.java
+++ item-service/src/main/java/com/microservices/item/service/ItemServiceImpl.java
@@ -4,6 +4,8 @@ import com.microservices.item.dto.ItemCreationDto;
 import com.microservices.item.dto.ItemDto;
 import com.microservices.item.entity.ItemEntity;
 import com.microservices.item.repository.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import java.util.List;
 
 @Service
 public class ItemServiceImpl implements ItemService{
+
+    private static final Logger logger = LoggerFactory.getLogger(ItemServiceImpl.class);
 
     private ItemRepository itemRepository;
 
@@ -27,27 +31,31 @@ public class ItemServiceImpl implements ItemService{
         itemRepository.findAll().forEach(item -> {
             items.add(convertToItemDto(item));
         });
-
+        logger.info("Returned list of all items");
         return items;
     }
 
     @Override
     public ItemDto getOne(long id) {
         ItemEntity item = itemRepository.findById(id).orElseThrow(RuntimeException::new);
+        logger.info("Returned item with id = {}", item.getEntityId());
         return convertToItemDto(item);
     }
 
     @Override
     public ItemDto create(ItemCreationDto itemCreationDto) {
-        return convertToItemDto(itemRepository.save(
-                new ItemEntity(itemCreationDto.getName(), itemCreationDto.getPrice(), itemCreationDto.getAmount()))
-        );
+        ItemEntity item = itemRepository.save(
+                new ItemEntity(itemCreationDto.getName(), itemCreationDto.getPrice(), itemCreationDto.getAmount()));
+        logger.info("Created item with id = {}", item.getEntityId());
+        return convertToItemDto(item)
+        ;
     }
 
     @Override
     public ItemDto increaseAmount(long id, long amount) throws RuntimeException {
         ItemEntity item = itemRepository.findById(id).orElseThrow(RuntimeException::new);
         item.setAvailableAmount(item.getAvailableAmount()+ amount);
+        logger.info("item with id = {} amount increased by {}", item.getEntityId(), amount);
         return convertToItemDto(itemRepository.save(item));
     }
 
@@ -55,6 +63,7 @@ public class ItemServiceImpl implements ItemService{
     public ItemDto decreaseAmount(long id, long amount) throws RuntimeException {
         ItemEntity item = itemRepository.findById(id).orElseThrow(RuntimeException::new);
         item.setAvailableAmount(item.getAvailableAmount() - amount);
+        logger.info("item with id = {} amount decreased by {}", item.getEntityId(), amount);
         return convertToItemDto(itemRepository.save(item));
     }
 
