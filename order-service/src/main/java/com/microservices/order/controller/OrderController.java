2b7c9a6ee41a689cca866dabbcb675f19bcd3422
Zalkin <Zalkin.viktor@mail.ru>
2019-11-08 18:31:42 +0300

    order.OrderController. Add RabbitListener and test sender

--- order-service/src/main/java/com/microservices/order/controller/OrderController.java
+++ order-service/src/main/java/com/microservices/order/controller/OrderController.java
@@ -5,15 +5,28 @@ import com.microservices.order.dto.OrderDto;
 import com.microservices.order.dto.UserDetailsDto;
 import com.microservices.order.entity.OrderStatus;
 import com.microservices.order.service.OrderService;
+import com.microservices.order.service.OrderServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
 
 import java.util.List;
 import java.util.Optional;
 
+@EnableRabbit
 @RestController
 @RequestMapping("api/orders")
 public class OrderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private OrderService orderService;
 
@@ -26,6 +39,7 @@ public class OrderController {
     public OrderDto getOrderById(
         @PathVariable int orderId){
 
+        testSend(orderId);
         return orderService.getOrderById(orderId);
     }
 
@@ -59,5 +73,17 @@ public class OrderController {
 
         orderService.getOrderById(orderId);
         throw new IllegalArgumentException("I need help of payment-service to perform payment, but I cannot communicate with it :(");
+    }
+
+    @RabbitListener(queues = "qorder")
+    public void handleMessage(String message) {
+       logger.info(message);
+    }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void testSend(int orderId) {
+        rabbitTemplate.convertAndSend("exchange", "order", "I am able to send message with orderId =" + orderId);
     }
 }
