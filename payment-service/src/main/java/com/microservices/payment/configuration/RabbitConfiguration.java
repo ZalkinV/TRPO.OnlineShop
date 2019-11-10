f14a4b1c11eed8c87f57c4dd4fd05dba9cfa3219
Zalkin <Zalkin.viktor@mail.ru>
2019-11-10 13:26:01 +0300

    payment. Configure RabbitMQ

---
+++ payment-service/src/main/java/com/microservices/payment/configuration/RabbitConfiguration.java
@@ -0,0 +1,31 @@
+package com.microservices.payment.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("directExchange");
+    }
+
+    @Bean
+    public Queue queue() {
+        return  new Queue("qpayment");
+    }
+
+    @Bean
+    public Binding binding(DirectExchange directExchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(directExchange).with("payment");
+    }
+
+}
--- payment-service/src/main/java/com/microservices/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/microservices/payment/controller/PaymentController.java
@@ -3,15 +3,23 @@ package com.microservices.payment.controller;
 import com.microservices.payment.dto.PaymentCreationDto;
 import com.microservices.payment.dto.PaymentDto;
 import com.microservices.payment.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 import javax.validation.Valid;
 
+@EnableRabbit
 @RestController
 @RequestMapping("payment")
 public class PaymentController {
     private PaymentService paymentService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
 
     @Autowired
     public PaymentController(PaymentService paymentService) {
@@ -38,4 +46,9 @@ public class PaymentController {
         return paymentService.cancelPayment(payment_id);
     }
 
+
+    @RabbitListener(queues = "qpayment")
+    public void cancelPayment(Message message) {
+        logger.info("*********************" + message.toString());
+    }
 }
